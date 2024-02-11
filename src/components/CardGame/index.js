367b2740521d5f1a58f281635a5d500708a0df40
BpeGabriel <bpgabriel611@gmail.com>
2024-02-11 12:02:08 -0300

    Creat board game

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,10 +2,20 @@ import "./style.css";
 
 function CardGame() {
   return `
-  <article class="card-game"> 
-    <img src="images/logo.png">
-  </article>
-  `;
-}
+  <div class= "CardGame flip">
+      <div class="face front">
+        <img src="images/logo.png">
+      </div>
+      <div class="face back">
+      <img src="images/logo.png">
+      </div>
+    </div>
 
+
+    `;
+}
+// const flipCard = document.querySelector(".CardGame");
+// flipCard.addEventListener("click", (e) => {
+//   flipCard.classList.toggle("flip");
+// });
 export default CardGame;
